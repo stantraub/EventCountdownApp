e229b3177fe563df233fb9b5b3e8ffaa6c0b5eaa
Stanley <stanley.traub@gmail.com>
2021-05-10 18:33:03 -0700

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Stanley Traub on 5/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
